cefcab14014f617bba10620b5029757f9eeb8aa0
chaoqun <chaoqun@pku.edu.cn>
2018-10-10 23:33:01 +0800

    网络更新信息（update weather info by network）

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,65 @@
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+}
 
 Page({
+  data: {
+    nowTemp: "",
+    nowWeather: "",
+    nowWeatherImg: '',
+  },
+  onPullDownRefresh(){
+    this.getNowInfo(()=>{
+      wx.stopPullDownRefresh()
+    })
+  },
+  onLoad(){
+    this.getNowInfo()
+  },
+  getNowInfo(callback){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '北京市'
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: res => {
+        console.log(res)
+        let result = res.data.result
+        let temp = result.now.temp
+        let weather = result.now.weather
+        console.log(temp, weather)
+        this.setData({
+          nowTemp: temp + '°',
+          nowWeather: weatherMap[weather],
+          nowWeatherImg: '/images/' + weather + '-bg.png',
+        })
+        wx.setNavigationBarColor({
+          frontColor: '#000000',
+          backgroundColor: weatherColorMap[weather],
+        })
+      },
+      complete: () => {
+        callback && callback()
+      }
+    })
+}
+
+  
 })
